e7579c949e6dfb5d07e5b7a148920fefb82a9dfd
Prueba git <danielesteban2004@gmail.com>
2024-08-18 04:08:10 -0500

    3x3 agregado

--- script/script.js
+++ script/script.js
@@ -36,10 +36,19 @@ function crearEcuacionesHTML(cantidad) {
     for (let i = 0; i < cantidad; i++) {
         const spanEcuacion = document.createElement("span");
         spanEcuacion.classList.add("ecuacion");
-        spanEcuacion.innerHTML = "Ecuación " + (i + 1) +
-            "<input type='number' class='inputEcuacion x'><span class='incognita'>x</span>" +
-            "<input type='number' class='inputEcuacion y '><span class='incognita'>y =</span>" +
-            "<input type='number' class='inputEcuacion independiente'>";
+        if (cantidad == 2) {
+            spanEcuacion.innerHTML = "Ecuación " + (i + 1) +
+                "<input type='number' class='inputEcuacion x'><span class='incognita'>x</span>" +
+                "<input type='number' class='inputEcuacion y '><span class='incognita'>y =</span>" +
+                "<input type='number' class='inputEcuacion independiente'>";
+        } else if (cantidad == 3) {
+            spanEcuacion.classList.add("ecuacionTres");
+            spanEcuacion.innerHTML = "Ecuación " + (i + 1) +
+                "<input type='number' class='inputEcuacion x'><span class='incognita'>x</span>" +
+                "<input type='number' class='inputEcuacion y '><span class='incognita'>y</span>" +
+                "<input type='number' class='inputEcuacion z '><span class='incognita'>z =</span>" +
+                "<input type='number' class='inputEcuacion independiente'>";
+        }
         divEcuaciones.append(spanEcuacion);
     }
 
@@ -62,12 +71,23 @@ function crearEcuacionesHTML(cantidad) {
         });
     });
 
+    //Mostrar "Z" si el sistema es 3x3 y ocultar si es 2X2
+    if (cantidad == 2) {
+        document.querySelectorAll(".ValorZ").forEach(cuadro => {
+            cuadro.classList.add("tres");
+        })
+    } else if (cantidad == 3) {
+        document.querySelectorAll(".ValorZ").forEach(cuadro => {
+            cuadro.classList.remove("tres");
+        })
+    }
+
 }
 
-function validarInputVacios(inputs){
+function validarInputVacios(inputs) {
     var vacios = false;
-    inputs.forEach(inpt =>{
-        if(inpt.value.trim() == ""){
+    inputs.forEach(inpt => {
+        if (inpt.value.trim() == "") {
             vacios = true;
         }
     })
@@ -80,49 +100,133 @@ function validarInputVacios(inputs){
 function calcularResultados() {
     window.location = '#contenedorDeltas'; // La versión movil hace scroll a los resultados
 
-    
+
     //Obtener variables necesarias
     todosLosValores = document.querySelectorAll("input");
     let CoeficientesX = [];
     let CoeficientesY = [];
+    let CoeficientesZ = [];
     let Independientes = [];
-    
+
     var inputVacios = validarInputVacios(todosLosValores); // Validar que no haya ningun campo de texto vacio
-    
+
     // Si no estan vacios los campos de texto
-    if(!inputVacios){
+    if (!inputVacios) {
         todosLosValores.forEach(elemento => {
             if (elemento.classList.contains("x")) {
                 CoeficientesX.push(parseFloat(elemento.value));
             } else if (elemento.classList.contains("y")) {
                 CoeficientesY.push(parseFloat(elemento.value));
+            } else if (elemento.classList.contains("z")) {
+                CoeficientesZ.push(parseFloat(elemento.value));
             } else {
                 Independientes.push(parseFloat(elemento.value));
             }
         });
 
-        console.log("Coeficientes X: " + CoeficientesX);
-        console.log("Coeficientes Y: " + CoeficientesY);
-        console.log("independientes: " + Independientes);
-        
-    
-        //Calculos
-    
-        let delta = (CoeficientesX[0] * CoeficientesY[1]) - (CoeficientesY[0] * CoeficientesX[1]);
-        let deltaX = (Independientes[0] * CoeficientesY[1]) - (CoeficientesY[0] * Independientes[1]);
-        let deltaY = 0
-        let valorX = (deltaX / delta);
-        let valorY = 0
-        
-    
+
+
         if (sistemaDeEcuaciones == 2) { //Si el sistema es 2x2
+
+            // Calculos 2x2
+
+            let delta = (CoeficientesX[0] * CoeficientesY[1]) - (CoeficientesY[0] * CoeficientesX[1]);
+            let deltaX = (Independientes[0] * CoeficientesY[1]) - (CoeficientesY[0] * Independientes[1]);
+            let deltaY = 0;
+            let valorX = (deltaX / delta);
+            let valorY = 0;
+
+            //Imprimir en pantalla
+
             document.querySelector("#Delta").innerHTML = "&#9651; = " + delta;
             document.querySelector("#DeltaX").innerHTML = "&#9651;x = " + deltaX;
             document.querySelector("#DeltaY").innerHTML = "&#9651;y = " + deltaY;
             document.querySelector("#ValorX").innerHTML = "x = " + valorX;
             document.querySelector("#ValorY").innerHTML = "y = " + valorY;
+
+            // Validaciones
+
+            if (delta == 0) {
+
+                if (deltaX == 0 && deltaY == 0) {
+                    document.querySelector("#Nota").innerHTML = "<b>El sistema es consistente y tiene infinitas soluciones</b>";
+                } else if (deltaX != 0 || deltaY != 0) {
+                    document.querySelector("#Nota").innerHTML = "<b>El sistema es inconsistente y no tiene solución</b>";
+                }
+
+            } else {
+                document.querySelector("#Nota").innerHTML = "<b>El sistema es consistente y tiene una única solución</b>";
+            }
+
+        } else if (sistemaDeEcuaciones == 3) { // Si el sistema es 3x3
+
+            // Calculos 3x3
+            var matrizDeltas = generarMatriz(CoeficientesX, CoeficientesY, CoeficientesZ);
+            let delta = multiplicarDiagonales(matrizDeltas);
+
+            matrizDeltas = generarMatriz(Independientes, CoeficientesY, CoeficientesZ);
+            let deltaX = multiplicarDiagonales(matrizDeltas);
+
+            let deltaY = 0;
+
+            let deltaZ = 0;
+
+            let valorX = deltaX / delta;
+
+            let valorY = 0;
+
+            let valorZ = 0;
+
+            //Imprimir en pantalla
+
+            document.querySelector("#Delta").innerHTML = "&#9651; = " + delta;
+            document.querySelector("#DeltaX").innerHTML = "&#9651;x = " + deltaX;
+            document.querySelector("#DeltaY").innerHTML = "&#9651;y = " + deltaY;
+            document.querySelector("#DeltaZ").innerHTML = "&#9651;z = " + deltaZ;
+            document.querySelector("#ValorX").innerHTML = "x = " + valorX;
+            document.querySelector("#ValorY").innerHTML = "y = " + valorY;
+            document.querySelector("#ValorZ").innerHTML = "z = " + valorZ;
+
+            // Validaciones
+
+            if (delta == 0) {
+
+                if (deltaX == 0 && deltaY == 0) {
+                    document.querySelector("#Nota").innerHTML = "<b>El sistema es consistente y tiene infinitas soluciones</b>";
+                } else if (deltaX != 0 || deltaY != 0) {
+                    document.querySelector("#Nota").innerHTML = "<b>El sistema es inconsistente y no tiene solución</b>";
+                }
+
+            } else {
+                document.querySelector("#Nota").innerHTML = "<b>El sistema es consistente y tiene una única solución</b>";
+            }
+
         };
-    } else{
+    } else {
         alert("No debe dejar ningún campo vacío")
     }
 };
+
+function generarMatriz(columna0, columna1, columna2) {
+    return [
+        [columna0[0], columna1[0], columna2[0]],
+        [columna0[1], columna1[1], columna2[1]],
+        [columna0[2], columna1[2], columna2[2]],
+        [columna0[0], columna1[0], columna2[0]],
+        [columna0[1], columna1[1], columna2[1]]
+    ];
+}
+
+function multiplicarDiagonales(matriz) {
+    resultado1 = 0, resultado2 = 0;
+
+    for (let i = 0; i < 3; i++) { // De izquierda a derecha
+        resultado1 += (matriz[i][0] * matriz[i + 1][1] * matriz[i + 2][2])
+    }
+
+    for (let i = 0; i < 3; i++) { // De derecha a izquierda
+        resultado2 += (matriz[i][2] * matriz[i + 1][1] * matriz[i + 2][0])
+    }
+
+    return resultado1 - resultado2
+}
